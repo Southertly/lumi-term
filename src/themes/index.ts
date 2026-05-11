d510100ee58b029974d38c4d61fe7e71f6584152
LumiTerm Dev <dev@lumiterm.local>
2026-05-11 16:49:50 +0800

    feat: include Warp-imported themes in theme list

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,5 +1,9 @@
 import { builtinThemes } from './builtin';
+import { warpImportedThemes } from './warp-imported';
 
 export type { AppTheme, AppThemeUi, TerminalTheme } from './types';
 
-export const allThemes = [...builtinThemes];
+export const allThemes = [
+  ...builtinThemes,
+  ...warpImportedThemes,
+];
